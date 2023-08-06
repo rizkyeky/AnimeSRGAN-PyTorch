7733da82da1d007c45247bd3d83786050a11e8f1
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2023-08-06 11:53:55 +0700

    update export

--- import_model.py
+++ import_model.py
@@ -2,6 +2,7 @@ import torch
 from PIL import Image
 import torchvision.transforms as transforms
 import onnxruntime as ort
+from time import time
 
 if __name__ == '__main__':
     
@@ -11,19 +12,30 @@ if __name__ == '__main__':
     image1 = Image.open('rose_32.jpg')
     image1 = transforms.ToTensor()(image1)
 
+    start = time()
     output = jit_model(image1)
-    output = transforms.ToPILImage()(output)
-    output.save('jit_output.jpg')
+    now = time()
+    print(round((now-start), 3))
+    # output = transforms.ToPILImage()(output)
+    # output.save('jit_output.jpg')
 
-    sess = ort.InferenceSession('animesr.onnx')
+    # sess_options = ort.SessionOptions()
+    # sess_options.log_severity_level = 0
+    sess = ort.InferenceSession('animesr.onnx', 
+                                providers=['CPUExecutionProvider'],
+                                # sess_options=sess_options
+                                )
     input_name = sess.get_inputs()[0].name
 
-    image1 = transforms.Resize((480,480))(image1)
+    image1 = transforms.Resize((224,224))(image1)
+    start = time()
     outputs = sess.run(None, {input_name: image1.numpy()})
+    now = time()
+    print(round((now-start), 3))
     output_tensor = outputs[0]
-    print(output_tensor.shape)
+    # print(output_tensor.shape)
 
-    output = transforms.ToPILImage()(torch.from_numpy(output_tensor))
-    output.save('onnx_output.jpg')
+    # output = transforms.ToPILImage()(torch.from_numpy(output_tensor))
+    # output.save('onnx_output.jpg')
 
 
